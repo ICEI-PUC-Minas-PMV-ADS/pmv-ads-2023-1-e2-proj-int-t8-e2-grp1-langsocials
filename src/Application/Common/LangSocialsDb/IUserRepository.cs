efa7eb4d670b98da168c1ec38e31f76585b47d72
Pedro Rafael <pedrorafael1830@gmail.com>
2023-05-08 22:48:47 -0300

    feat: implementando metodos de userRepository

---
+++ src/Application/Common/LangSocialsDb/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using LangSocials.Domain.Entities;
+
+namespace Application.Common.LangSocialsDb
+{
+    public interface IUserRepository
+    {
+        void Update(User user);
+        Task<bool> IsUniqueName(string name, CancellationToken cancellationToken);
+        Task<bool> IsUniqueEmail(string email, CancellationToken cancellationToken);
+        Task<User?> Find(int id, CancellationToken cancellationToken);
+    }
+}
---
+++ src/Infraesctructure/LangSocialsDb/Repository/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿using Application.Common.LangSocialsDb;
+using LangSocials.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LangSocials.Infraesctructure.LangSocialsDb.Repository;
+
+public class UserRepository : IUserRepository
+{
+    private readonly LangSocialsDbContext context;
+
+    public UserRepository(LangSocialsDbContext context)
+    {
+        this.context = context;
+    }
+
+    public async Task<User?> Find(int id, CancellationToken cancellationToken)
+       => await context.Users.FirstOrDefaultAsync(a => a.Id == id, cancellationToken);
+    public async Task<bool> IsUniqueName(string name, CancellationToken cancellationToken)
+       => await context.Users.AnyAsync(a => a.Name == name, cancellationToken);
+    public async Task<bool> IsUniqueEmail(string email, CancellationToken cancellationToken)
+       => await context.Users.AnyAsync(a => a.Email == email, cancellationToken);
+    public void Update(User user)
+       => context.Update(user);
+}
