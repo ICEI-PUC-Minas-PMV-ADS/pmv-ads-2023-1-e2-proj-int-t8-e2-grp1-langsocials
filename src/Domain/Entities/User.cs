095f8535eb7290ef34871fa1c3f37de5d0476566
Pedro Rafael <pedrorafael1830@gmail.com>
2023-05-08 22:48:30 -0300

    feat: corrigindo mapping de user

--- src/Domain/Entities/User.cs
+++ src/Domain/Entities/User.cs
@@ -3,9 +3,11 @@
 public class User : Entity
 {
     public required string Name { get; set; }
+    public required string Email { get; set; }
     public required byte[] PasswordHash { get; set; }
     public required byte[] PasswordSalt { get; set; }
     public bool ShowContactInfo { get; set; }
+    public required string Description { get; set; }
     public required ICollection<SocialEvent> Organizing { get; set; }
     public required ICollection<SocialEvent> Participating { get; set; }
 }
