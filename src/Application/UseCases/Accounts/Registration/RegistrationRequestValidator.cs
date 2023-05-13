f8722a6ee0adb05397adf1d7c4239d4c8b340ede
Luiz-Ossinho <luizeduardodejes@gmail.com>
2023-05-12 21:25:54 -0300

    feat: Adicionando validacao de email

--- src/Application/UseCases/Accounts/Registration/RegistrationRequestValidator.cs
+++ src/Application/UseCases/Accounts/Registration/RegistrationRequestValidator.cs
@@ -7,8 +7,8 @@ public class RegistrationRequestValidator : AbstractValidator<RegistrationReques
 	public RegistrationRequestValidator()
 	{
 		RuleFor(r => r.Name).NotEmpty().MinimumLength(2).MaximumLength(200);
-        RuleFor(r => r.Email).NotEmpty().MinimumLength(2).MaximumLength(200); // TODO: Implementar validacao correta de email
-        RuleFor(r => r.Password).NotEmpty().MinimumLength(2).MaximumLength(32);// TODO: Implementar validacao correta de senha
+        RuleFor(r => r.Email).NotEmpty().EmailAddress().MinimumLength(2).MaximumLength(200);
+        RuleFor(r => r.Password).NotEmpty().MinimumLength(2).MaximumLength(32); // TODO: Implementar validacao correta de senha
         RuleFor(r => r.Description).MaximumLength(200);
     }
 }
