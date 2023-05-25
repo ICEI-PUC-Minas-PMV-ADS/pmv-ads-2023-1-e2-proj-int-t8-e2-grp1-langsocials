7a2a2773719f0a3cdc292b23cb6e6f12b97a3daf
Luiz-Ossinho <luizeduardodejes@gmail.com>
2023-05-25 20:20:45 -0300

    fix: Erro de compilacao

--- src/Application/UseCases/SocialEvents/GetSocialEvents/GetSocialEventsRequest.cs
+++ src/Application/UseCases/SocialEvents/GetSocialEvents/GetSocialEventsRequest.cs
@@ -1,43 +1,28 @@
 ﻿using Application.Common.LangSocialsDb;
 using Application.Common.MediatrExtensions;
-using Application.Common.Services;
 using AutoMapper;
 using FluentResults;
-using LangSocials.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.UseCases.SocialEvents.GetSocialEvents;
 
-public record GetSocialEventsRequest(Location? SearchLocation) : IResultRequest<IEnumerable<GetSocialEventsResponse>>;
+public record GetSocialEventsRequest(string CityFilter, string StateFilter) : IResultRequest<IEnumerable<GetSocialEventsResponse>>;
 
 public class GetSocialEventsRequestHandler : IResultRequestHandler<GetSocialEventsRequest, IEnumerable<GetSocialEventsResponse>>
 {
-    const double MaxDistance = 50000;
     private readonly ISocialEventsRepository socialEventsRepository;
-    private readonly IUserInfo userInfo;
     private readonly IMapper mapper;
 
-    public GetSocialEventsRequestHandler(ISocialEventsRepository socialEventsRepository, IUserInfo userInfo, IMapper mapper)
+    public GetSocialEventsRequestHandler(ISocialEventsRepository socialEventsRepository, IMapper mapper)
     {
         this.socialEventsRepository = socialEventsRepository;
-        this.userInfo = userInfo;
         this.mapper = mapper;
     }
 
     public async Task<Result<IEnumerable<GetSocialEventsResponse>>> Handle(GetSocialEventsRequest request, CancellationToken cancellationToken)
     {
-        var searchLocation = request.SearchLocation ?? userInfo.SearchLocation;
-        if (searchLocation is null)
-            return Result.Fail(new UnhandledError()); // TODO: Refatorar tratamento de erro
-
-        var queriedEvents = socialEventsRepository.QuerySocialEvents((searchLocation.Latitude, searchLocation.Longitude, MaxDistance), cancellationToken: cancellationToken);
+        var queriedEvents = socialEventsRepository.QuerySocialEvents(request.CityFilter, request.StateFilter, cancellationToken: cancellationToken);
 
         var mappedEvents = mapper.Map<IEnumerable<GetSocialEventsResponse>>(queriedEvents); // TODO: Implementar mapa
-        // TODO: Repensar fluxo de localizacao
 
         return Result.Ok(mappedEvents);
     }
