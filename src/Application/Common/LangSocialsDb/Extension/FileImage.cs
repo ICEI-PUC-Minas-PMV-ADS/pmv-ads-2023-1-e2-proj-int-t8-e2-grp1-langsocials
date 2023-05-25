a028be0b713caffbd0082927def7dbf4d3e62c3f
Pedro Rafael <pedrorafael1830@gmail.com>
2023-05-25 00:55:41 -0300

    Implementando FileRepository e FileImage

---
+++ src/Application/Common/LangSocialsDb/Extension/FileImage.cs
@@ -0,0 +1,3 @@
+﻿namespace Application.Common.LangSocialsDb.Extension;
+
+public record FileImage(string Extention, string ImageName, byte[] Image);
---
+++ src/Application/Common/LangSocialsDb/IFileRepository.cs
@@ -0,0 +1,10 @@
+﻿using Application.Common.LangSocialsDb.Extension;
+
+namespace Application.Common.LangSocialsDb;
+
+public interface IFileRepository
+{
+    string UserDefaultImage { get; }
+    string UpdateUserImage(string lastImage, FileImage fileImage);
+    void DeleteImage(string imageURI);
+}
