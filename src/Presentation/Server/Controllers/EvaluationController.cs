81e76d8bc6da5fe9c238e2cd0235ab093d3dc003
Adeilton-Ro <adeiltonjr15@gmail.com>
2023-05-25 21:46:45 -0300

    Criando endpoint da feature de avalidação

---
+++ src/Presentation/Server/Controllers/EvaluationController.cs
@@ -0,0 +1,24 @@
+﻿using Application.UseCases.Evaluation;
+using LangSocials.Presentation.Server.Extensions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LangSocials.Presentation.Server.Controllers;
+[Route("[Controller]")]
+public class EvaluationController : Controller
+{
+    private readonly ISender sender;
+
+    public EvaluationController(ISender sender)
+    {
+        this.sender = sender;
+    }
+
+    [HttpPost]
+    public async Task<IResult> Rating(RatingRequest request, CancellationToken cancellationToken)
+    {
+        var result = await sender.Send(request, cancellationToken);
+
+        return result.Serialize();
+    }
+}
