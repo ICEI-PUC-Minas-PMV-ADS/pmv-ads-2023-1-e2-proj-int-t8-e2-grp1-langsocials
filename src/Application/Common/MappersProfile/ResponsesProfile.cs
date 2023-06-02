800fcab5934ab72811691912092df721fefc82de
Adeilton-Ro <adeiltonjr15@gmail.com>
2023-06-01 22:12:06 -0300

    Criando profile para mapear socialevent para response da feature

---
+++ src/Application/Common/MappersProfile/ResponsesProfile.cs
@@ -0,0 +1,13 @@
+﻿using Application.UseCases.SocialEvents.GetSocialEventInformation;
+using AutoMapper;
+using LangSocials.Domain.Entities;
+
+namespace Application.Common.MappersProfile;
+public class ResponsesProfile : Profile
+{
+    public ResponsesProfile()
+    {
+        CreateMap<SocialEvent, GetSocialEventInformationResponse>()
+            .ForCtorParam("Tags", cese => cese.MapFrom(se => se.Tags));
+    }
+}
