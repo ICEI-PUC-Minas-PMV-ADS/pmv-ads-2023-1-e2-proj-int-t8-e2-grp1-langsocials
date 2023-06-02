ed9b622d8d7004090b97be91ff7fe67e30fd8614
Adeilton-Ro <adeiltonjr15@gmail.com>
2023-06-01 22:12:15 -0300

    Adicionando controller

---
+++ src/Presentation/Server/Controllers/SocialEventController.cs
@@ -0,0 +1,23 @@
+﻿using Application.UseCases.SocialEvents.GetSocialEventInformation;
+using LangSocials.Presentation.Server.Extensions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LangSocials.Presentation.Server.Controllers;
+public class SocialEventController : ControllerBase
+{
+    private readonly ISender sender;
+
+    public SocialEventController(ISender sender)
+    {
+        this.sender = sender;
+    }
+
+    [HttpGet("{Id}")]
+    public async Task<IResult> GetInformations([FromRoute] uint Id, CancellationToken cancellationToken)
+    {
+        var result = await sender.Send(new GetSocialEventInformationRequest(Id), cancellationToken);
+
+        return result.Serialize();
+    }
+}
