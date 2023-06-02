f4289dc72bb61976abec138dc8b96510880634c8
Adeilton-Ro <adeiltonjr15@gmail.com>
2023-06-01 22:11:00 -0300

    Adicionando metodo de buscar evendo por id

---
+++ src/Infraesctructure/LangSocialsDb/Repository/SocialEventRepository.cs
@@ -0,0 +1,23 @@
+﻿using Application.Common.LangSocialsDb;
+using LangSocials.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LangSocials.Infraesctructure.LangSocialsDb.Repository;
+public class SocialEventRepository : ISocialEventsRepository
+{
+    private readonly LangSocialsDbContext context;
+
+    public SocialEventRepository(LangSocialsDbContext context)
+    {
+        this.context = context;
+    }
+    public Task<SocialEvent?> QueryById(uint id, CancellationToken cancellationToken = default)
+    {
+        return context.SocialEvents.Include(se => se.Tags).SingleOrDefaultAsync(se => se.Id == id);
+    }
+
+    public Task<IEnumerable<SocialEvent>> QuerySocialEvents(string? cityFilter = null, string? stateFilter = null, bool orderByPopularity = false, int take = 20, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
