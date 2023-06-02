db34bb5cd76c41ac23fda50177018069c976e864
Adeilton-Ro <adeiltonjr15@gmail.com>
2023-06-01 22:40:22 -0300

    Adicionando Controller

---
+++ src/Presentation/Server/Controllers/LocationController.cs
@@ -0,0 +1,23 @@
+﻿using Application.UseCases.Locations.GetLocation;
+using LangSocials.Presentation.Server.Extensions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LangSocials.Presentation.Server.Controllers;
+public class LocationController : ControllerBase
+{
+    private readonly ISender sender;
+
+    public LocationController(ISender sender)
+    {
+        this.sender = sender;
+    }
+
+    [HttpGet("{Id}")]
+    public async Task<IResult> GetLocation(int Id, CancellationToken cancellationToken)
+    {
+        var result = await sender.Send(new GetLocationRequest(Id), cancellationToken);
+
+        return result.Serialize();
+    }
+}
